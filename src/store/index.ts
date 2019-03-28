32d799584e0e212a79b9dbcfe89898a8c6d95260
Paul Giletich <paul.giletich@gmail.com>
2019-03-28 20:10:47 +0300

    EPMCUXDA-6481 - add redux devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,12 @@
 import { applyMiddleware, createStore } from "redux";
 import { StateType } from "typesafe-actions";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { rootReducer } from "./rootReducer";
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export type RootState = StateType<typeof rootReducer>;
