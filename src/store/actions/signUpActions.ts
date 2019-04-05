8b2f59df871f23ac99e9d57c37944b4c1e37e4f5
Paul Giletich <paul.giletich@gmail.com>
2019-04-05 15:34:31 +0300

    fix typescript complains

--- src/store/actions/signUpActions.ts
+++ src/store/actions/signUpActions.ts
@@ -1,27 +1,25 @@
-import { createAsyncAction } from "typesafe-actions";
 import { ThunkAction } from "redux-thunk";
+import { createAction } from "redux-actions";
 import { SignUpData } from "../../app/features/auth/signup/SignUpData";
 import { RootState } from "../index";
 
-export const signUpAction = createAsyncAction(
-  "SIGN_UP_REQUEST",
-  "SIGN_UP_SUCCESS",
-  "SIGN_UP_FAILURE"
-)<SignUpData, any, string>();
+export const signUpRequest = createAction<SignUpData>("SIGN_UP_REQUEST");
+export const signUpSuccess = createAction<void>("SIGN_UP_SUCCESS");
+export const signUpFailure = createAction<string>("SIGN_UP_FAILURE");
 
 export const signUp = (
   data: SignUpData
 ): ThunkAction<void, RootState, undefined, any> => dispatch => {
-  dispatch(signUpAction.request(data));
+  dispatch(signUpRequest(data));
 
   new Promise<number>((resolve, reject) => {
     // replace this promise with api call
     reject(new Error("not implemented"));
   })
     .then(d => {
-      dispatch(signUpAction.success(d));
+      dispatch(signUpSuccess());
     })
     .catch((e: Error) => {
-      dispatch(signUpAction.failure(e.message));
+      dispatch(signUpFailure(e.message));
     });
 };
