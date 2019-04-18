491c6f3329dd0e006fe4c5bb3af5b54b590160c8
Paul Giletich <paul.giletich@gmail.com>
2019-04-18 15:06:18 +0300

    fix: remove unnecessary api root as all the requests are passiing through proxy from package.json

--- src/config/endpoints.ts
+++ src/config/endpoints.ts
@@ -1,9 +1,7 @@
-const root = "http://localhost:3001";
-
 /**
  * Authorization/authentication
  */
 const authRoute = "auth";
-export const SING_UP_ENDPOINT = `${root}/${authRoute}/signup`;
-export const SING_IN_ENDPOINT = `${root}/${authRoute}/login`;
-export const RESTORE_PASSWORD_ENDPOINT = `${root}/${authRoute}/resetpassword`;
+export const SING_UP_ENDPOINT = `/${authRoute}/signup`;
+export const SING_IN_ENDPOINT = `/${authRoute}/login`;
+export const RESTORE_PASSWORD_ENDPOINT = `/${authRoute}/resetpassword`;
