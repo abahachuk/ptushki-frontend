93cba6aa763ab0e2048d8a0bf445d1e7e4d56d4c
Yana_Karotkina <yana_karotkina@epam.com>
2019-04-26 15:11:38 +0300

    EPMCUXDA-6553 (Auth integration) - rebase

--- src/app/features/nav/RootNav.tsx
+++ src/app/features/nav/RootNav.tsx
@@ -29,26 +29,27 @@ export const RootNav: FC<{
   if (!isLoaded) return null;
 
   return (
-  <Navbar dark expand="sm" className="py-1 px-5 root-nav">
-    <NavbarBrand href="/">
-      <img src={brandLogo} alt={labels.brandName} />
-    </NavbarBrand>
-    <Collapse isOpen navbar>
-      {user ? (
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink tag={Link} to={ROUTE_SIGN_UP.path}>
-              <img
-                src="http://i.pravatar.cc/60" // TODO this is a placeholder
-                alt="avatar"
-                className="nav-user-img"
-              />
-            </NavLink>
-          </NavItem>
-        </Nav>
-      ) : (
-        <NavUnauthorized />
-      )}
-    </Collapse>
-  </Navbar>
-)};
+    <Navbar dark expand="sm" className="py-1 px-5 root-nav">
+      <NavbarBrand href="/">
+        <img src={brandLogo} alt={labels.brandName} />
+      </NavbarBrand>
+      <Collapse isOpen navbar>
+        {user ? (
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink tag={Link} to={ROUTE_SIGN_UP.path}>
+                <img
+                  src="http://i.pravatar.cc/60" // TODO this is a placeholder
+                  alt="avatar"
+                  className="nav-user-img"
+                />
+              </NavLink>
+            </NavItem>
+          </Nav>
+        ) : (
+          <NavUnauthorized />
+        )}
+      </Collapse>
+    </Navbar>
+  );
+};
