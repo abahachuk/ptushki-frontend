9f120bfeb6d829367d45826da247c16e62766d1d
Paul Giletich <paul.giletich@gmail.com>
2019-04-26 12:51:19 +0300

    set header color and add logged-in user picture placeholder

--- src/app/features/nav/RootNav.tsx
+++ src/app/features/nav/RootNav.tsx
@@ -1,16 +1,46 @@
 import React, { FC } from "react";
-import { Collapse, Navbar, NavbarBrand } from "reactstrap";
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavItem,
+  NavLink
+} from "reactstrap";
+import { NavLink as Link } from "react-router-dom";
 
 import { NavUnauthorized } from "./NavUnauthorized";
 import { UserInfo } from "../auth/models";
+import { labels } from "../../../config/i18n/labels";
+import { ROUTE_SIGN_UP } from "../routing/routes";
+
+import "./RootNav.scss";
+
+const brandLogo = require("../../../assets/brand-logo.svg");
 
 export const RootNav: FC<{
   user: UserInfo;
 }> = ({ user }) => (
-  <Navbar dark color="dark" expand="sm">
-    <NavbarBrand href="/">PTUSHKI</NavbarBrand>
+  <Navbar dark expand="sm" className="py-1 px-5 root-nav">
+    <NavbarBrand href="/">
+      <img src={brandLogo} alt={labels.brandName} />
+    </NavbarBrand>
     <Collapse isOpen navbar>
-      {user ? <div /> : <NavUnauthorized />}
+      {user ? (
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink tag={Link} to={ROUTE_SIGN_UP.path}>
+              <img
+                src="http://i.pravatar.cc/60" // TODO this is a placeholder
+                alt="avatar"
+                className="nav-user-img"
+              />
+            </NavLink>
+          </NavItem>
+        </Nav>
+      ) : (
+        <NavUnauthorized />
+      )}
     </Collapse>
   </Navbar>
 );
