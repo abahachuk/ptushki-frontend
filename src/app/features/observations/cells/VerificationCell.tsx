06d1fc17414da8fc8ecffd430702f50168bf495e
Paul Giletich <paul.giletich@gmail.com>
2019-05-17 14:55:47 +0300

    fix (VerificationCell): adjust styling

--- src/app/features/observations/cells/VerificationCell.tsx
+++ src/app/features/observations/cells/VerificationCell.tsx
@@ -1,45 +1,35 @@
-import React from "react";
 import { Clear, Done } from "@material-ui/icons";
-import { Button, ButtonGroup } from "reactstrap";
+import React from "react";
 import { connect, DispatchProp } from "react-redux";
-import { TmpObservation } from "../../../../store/reducers/observationListReducer";
-import { labels } from "../../../../config/i18n/labels";
+import { Button, ButtonGroup } from "reactstrap";
 import { verifyObservation } from "../../../../store/actions/observationListActions";
+import { TmpObservation } from "../../../../store/reducers/observationListReducer";
 
 export const VerificationCell = connect()(
   ({ dispatch, observation }: DispatchProp & { observation: TmpObservation }) =>
-    observation.verified ? (
-      <span>{labels.yes}</span>
-    ) : (
-      <>
-        <span className="pr-3">{labels.no}</span>
-        <ButtonGroup size="sm" className="m-n2">
-          <Button
-            outline
-            color="success"
-            className="border-0 border-light border-right"
-            onClick={() =>
-              dispatch(
-                verifyObservation({ id: observation.id, approved: true })
-              )
-            }
-          >
-            <Done />
-          </Button>
-          <div className="border-right" />
-          <Button
-            outline
-            color="danger"
-            className="border-0"
-            onClick={() =>
-              dispatch(
-                verifyObservation({ id: observation.id, approved: false })
-              )
-            }
-          >
-            <Clear />
-          </Button>
-        </ButtonGroup>
-      </>
+    !observation.verified && (
+      <ButtonGroup size="sm" className="m-n2">
+        <Button
+          outline
+          color="success"
+          className="border-0 border-light border-right"
+          onClick={() =>
+            dispatch(verifyObservation({ id: observation.id, approved: true }))
+          }
+        >
+          <Done />
+        </Button>
+        <div className="border-right" />
+        <Button
+          outline
+          color="danger"
+          className="border-0"
+          onClick={() =>
+            dispatch(verifyObservation({ id: observation.id, approved: false }))
+          }
+        >
+          <Clear />
+        </Button>
+      </ButtonGroup>
     )
 );
