5122d21b1b10c847acc09cb74c2a5dd54bae6b4d
Dzianis_Shutikov <Dzianis_Shutikov@epam.com>
2019-05-19 23:19:25 +0300

    EPMCUXDA-6836: Preparation for deployment. Changed endpoints for NODE_ENV=production mode

--- src/config/endpoints.ts
+++ src/config/endpoints.ts
@@ -1,8 +1,9 @@
 /**
  * Authorization/authentication
  */
+const startURL = process.env.NODE_ENV === "production" ? "/api" : "";
 const authRoute = "auth";
-export const SING_UP_ENDPOINT = `/${authRoute}/signup`;
-export const SING_IN_ENDPOINT = `/${authRoute}/login`;
-export const RESTORE_PASSWORD_ENDPOINT = `/${authRoute}/resetpassword`;
-export const REFRESH_ENDPOINT = `/${authRoute}/refresh`;
+export const SING_UP_ENDPOINT = `${startURL}/${authRoute}/signup`;
+export const SING_IN_ENDPOINT = `${startURL}/${authRoute}/login`;
+export const RESTORE_PASSWORD_ENDPOINT = `${startURL}/${authRoute}/resetpassword`;
+export const REFRESH_ENDPOINT = `${startURL}/${authRoute}/refresh`;
