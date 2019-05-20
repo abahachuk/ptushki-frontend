b6b57dabe070dd89cd01661061d7afebe4afc84c
Paul Giletich <paul.giletich@gmail.com>
2019-05-20 20:57:21 +0300

    refactor (grid): un-freaking-believable piece of crap vertical scrollbar (add comment)

--- src/components/table/customisations/TableComponent.tsx
+++ src/components/table/customisations/TableComponent.tsx
@@ -5,6 +5,9 @@ import { CustomScrollContainer } from "../../scrollbars/CustomScrollContainer";
 
 export const TableComponent: FC<TableProps> = p => (
   <CustomScrollContainer>
+    {/* TODO the goddamn vertical scrollbar keeps appearing for no reason */}
+    {/*  check with designers on page layout, */}
+    {/*  maybe this piece of crap vertical scroll can be avoided */}
     <Table.Table
       {...p}
       className={classNames(
