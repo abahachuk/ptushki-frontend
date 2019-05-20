2bbb222dd9fb4bb0b3dfbc46a7dc935e60128924
Paul Giletich <paul.giletich@gmail.com>
2019-05-20 15:53:29 +0300

    fix (observations): new verification cell style

--- src/app/features/observations/cells/VerificationCell.tsx
+++ src/app/features/observations/cells/VerificationCell.tsx
@@ -1,35 +1,25 @@
-import { Clear, Done } from "@material-ui/icons";
+import { IconButton } from "@material-ui/core";
+import { CheckCircle } from "@material-ui/icons";
 import React from "react";
 import { connect, DispatchProp } from "react-redux";
-import { Button, ButtonGroup } from "reactstrap";
-import { verifyObservation } from "../../../../store/actions/observationListActions";
 import { TmpObservation } from "../../../../store/reducers/observationListReducer";
 
 export const VerificationCell = connect()(
-  ({ dispatch, observation }: DispatchProp & { observation: TmpObservation }) =>
-    !observation.verified && (
-      <ButtonGroup size="sm" className="m-n2">
-        <Button
-          outline
-          color="success"
-          className="border-0 border-light border-right"
-          onClick={() =>
-            dispatch(verifyObservation({ id: observation.id, approved: true }))
-          }
-        >
-          <Done />
-        </Button>
-        <div className="border-right" />
-        <Button
-          outline
-          color="danger"
-          className="border-0"
-          onClick={() =>
-            dispatch(verifyObservation({ id: observation.id, approved: false }))
-          }
-        >
-          <Clear />
-        </Button>
-      </ButtonGroup>
-    )
+  ({
+    dispatch,
+    observation
+  }: DispatchProp & { observation: TmpObservation }) => (
+    // TODO update api and wire up
+    <>
+      <IconButton className="p-0 mx-1 text-primary" disableRipple>
+        <CheckCircle />
+      </IconButton>
+      <IconButton className="p-0 mx-1 text-danger" disableRipple>
+        <CheckCircle />
+      </IconButton>
+      <IconButton className="p-0 mx-1 text-success" disableRipple>
+        <CheckCircle />
+      </IconButton>
+    </>
+  )
 );
