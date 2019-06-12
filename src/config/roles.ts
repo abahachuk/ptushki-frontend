f53454a2f867471e494da2fce0d9fa3748069f22
Yana_Karotkina <yana_karotkina@epam.com>
2019-06-12 23:31:05 +0300

    Data grid filters - rebase

--- src/config/roles.ts
+++ src/config/roles.ts
@@ -1,2 +1,6 @@
 export const UNAUTHORIZED = null as string;
 export const OBSERVER = "observer";
+export const RINGER = "ringer";
+export const SCIENTIST = "scientist";
+export const MODERATOR = "moderator";
+export const ADMIN = "admin";
