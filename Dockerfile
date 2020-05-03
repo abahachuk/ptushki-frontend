bf86cd303b7ebfba6890f1b9d9d39e3fb596fda0
Yauheni Serafimovich <yauheni_serafimovich@epam.com>
2020-05-03 18:08:18 +0300

    clear redundant files

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM nginx:1.13-alpine
-
-COPY nginx/ /etc/nginx
-COPY build/* /usr/share/nginx/www/
-COPY build/static/ /usr/share/nginx/www/static/
-
-expose 8080
--- build.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-npm install
-npm run build
-docker build -t ptushki-fe .
-docker tag ptushki-fe:latest ptushkiregistry.azurecr.io/ptushki-fe:latest
-docker login ptushkiregistry.azurecr.io
-docker push ptushkiregistry.azurecr.io/ptushki-fe:latest
